44f6d43cce01508ab4e604c4c0082d6ba19cc666
Carlos Andrés González Miranda <cargomida@gmail.com>
2026-05-15 17:41:10 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -236,10 +236,10 @@ def barra_superior():
     else:
         titulo = "Perfil administrador"
 
-    col_logo, col_titulo, col_boton = st.columns([1, 4, 1])
+    col_logo, col_titulo, col_boton = st.columns([1, 6, 1])
 
     with col_logo:
-        st.image("logo.png", width=150)
+        st.image("logo.png", width=200)
 
     with col_titulo:
         st.markdown(
