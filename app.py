0742008a095b1d175d97ec47b5c6d6e322b1559e
Carlos Andrés González Miranda <cargomida@gmail.com>
2026-05-15 17:10:59 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -243,14 +243,14 @@ def barra_superior():
 
     with col_titulo:
         st.markdown(
-            f"<h1 style='margin-top:25px;'>{titulo}</h1>",
+            f"<h1 style='margin-top:30px;'>{titulo}</h1>",
             unsafe_allow_html=True
         )
 
     with col_boton:
         st.markdown(
             """
-            <div style='height:50px;'></div>
+            <div style='height:70px;'></div>
             """,
             unsafe_allow_html=True
         )
